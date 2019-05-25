c40608631c0b0e06a077ee043a74c848f42e8a2f
PanchenkoNata <natalitsalucru@gmail.com>
2019-05-25 14:54:00 +0300

    front: add sort, add discount on the pr-card

---
+++ public/javascripts/catalog.js
@@ -0,0 +1,13 @@
+
+
+// axios.get('/')
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // const updated_category = document.querySelector('input[name=updated_cat_name]');
+    // const updated_category_name = localStorage.getItem('updatedCategoryName');
+    
+
+
+});
